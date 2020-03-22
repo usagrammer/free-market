811016e275e88e0278c002c4b5e1bcd651e20722
Kazushi Yamanishi <keironia187149@gmail.com>
2020-03-22 21:23:09 +0900

    fix items_edit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,3 +1,15 @@
 class ItemsController < ApplicationController
 
+  def new
+    render layout: 'no_menu' # レイアウトファイルを指定
+  end
+
+  def edit
+    render layout: 'no_menu' # レイアウトファイル指定
+  end
+
+  def purchase_confirmation
+    render layout: 'no_menu' # レイアウトファイル指定
+  end
+
 end
