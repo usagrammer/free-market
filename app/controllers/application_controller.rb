d243836a0ce66d2af7feed485bc4392f3aa475f2
usagrammer <48462721+usagrammer@users.noreply.github.com>
2020-04-01 17:10:30 +0900

    set devise strong paramaters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
-
   before_action :basic_auth, if: :production?
+  before_action :configure_permitted_paramaters, if: :devise_controller?
 
   private
 
@@ -12,6 +12,10 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |username, password|
       username == Rails.application.credentials.basic[:user_name] && password == Rails.application.credentials.basic[:password]
     end
+  end
+
+  def configure_permitted_paramaters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :first_name_reading, :last_name, :last_name_reading, :birthday])
   end
 
 end
