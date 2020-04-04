3d39995c17b21489b430e8b90f16599def9035a4
usagrammer <48462721+usagrammer@users.noreply.github.com>
2020-04-04 19:05:18 +0900

    add message option to verify_recaptcha

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -76,7 +76,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   private
   def check_recaptcha
-    redirect_to new_user_registration_path unless verify_recaptcha
+    redirect_to new_user_registration_path unless verify_recaptcha(message: "reCAPTCHAを承認してください")
   end
 
 end
