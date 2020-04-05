d3e88c2a98fa19f65b1c9c4075076522cfde7c24
usagrammer <48462721+usagrammer@users.noreply.github.com>
2020-04-05 16:02:24 +0900

    set create action & strong paramater

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,12 +5,36 @@ class ItemsController < ApplicationController
     render layout: 'no_menu' # レイアウトファイルを指定
   end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save!
+      redirect_to root_path, notice: "出品に成功しました"
+    else
+      render :new
+    end
+  end
+
   def edit
     render layout: 'no_menu' # レイアウトファイル指定
   end
 
   def purchase_confirmation
     render layout: 'no_menu' # レイアウトファイル指定
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(
+      :name,
+      :price,
+      :detail,
+      :condition,
+      :delivery_fee_payer,
+      :delivery_method,
+      :delivery_days,
+      :prefecture_id,
+      :category_id
+      ).merge(seller_id: current_user.id)
   end
 
 end
