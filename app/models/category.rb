1152c8e9a630e539fff2df5b8fd52138e4309ae3
usagrammer <48462721+usagrammer@users.noreply.github.com>
2020-04-05 16:00:19 +0900

    set association

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,1 +1,6 @@
+class Category < ApplicationRecord
+
   validates :name, presence: true
+
+  has_many :items
+end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,7 @@
   validates :name, :price, :detail, :condition, :delivery_fee_payer, :delivery_method, :prefecture_id, :delivery_days, :deal, presence: true
   validates :price, numericality:{greater_than_or_equal_to: 300,less_than_or_equal_to: 9999999}
+  belongs_to :category
+  belongs_to :seller, class_name: "User", foreign_key: 'seller_id'
+  belongs_to :buyer, class_name: "User", foreign_key: 'buyer_id', optional: :true
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
