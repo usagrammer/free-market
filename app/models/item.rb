4090b59ccd74765d138e7ad77471b0772f78ff48
usagrammer <48462721+usagrammer@users.noreply.github.com>
2020-04-06 13:18:49 +0900

    remove option forign_key

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,8 +4,8 @@ class Item < ApplicationRecord
   validates :price, numericality:{greater_than_or_equal_to: 300,less_than_or_equal_to: 9999999}
 
   belongs_to :category
-  belongs_to :seller, class_name: "User", foreign_key: 'seller_id'
-  belongs_to :buyer, class_name: "User", foreign_key: 'buyer_id', optional: :true
+  belongs_to :seller, class_name: "User"
+  belongs_to :buyer, class_name: "User", optional: :true
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
