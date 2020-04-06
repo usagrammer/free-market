5abf72adde6f558a95cb8edcb6387532871f733e
usagrammer <48462721+usagrammer@users.noreply.github.com>
2020-04-06 22:47:10 +0900

    add model option for move create

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,5 +1,7 @@
 class CardsController < ApplicationController
   def new
+    @card = Card.new
+
     now_year = Time.current.year        ## 現在の年を取得
     @exp_years = []                     ## これを年の選択肢として使用
     6.times do |i|                      ## 5回繰り返す。iは繰り返す毎に1増える
