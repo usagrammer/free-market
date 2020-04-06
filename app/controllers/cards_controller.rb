b5cfa2f0c38a046725523f2ca9dfb64c23127e53
usagrammer <48462721+usagrammer@users.noreply.github.com>
2020-04-06 22:48:01 +0900

    add create action

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -8,4 +8,20 @@ class CardsController < ApplicationController
       year = now_year + i               ## 2020,2021,2022..といった値を作る
       @exp_years << [year % 100, year]  ## [20, 2020]といった配列を作成して@exp_yearsに入れる
     end
+  end
+
+  def create
+    Payjp.api_key = Rails.application.credentials.payjp[:secret_key]
+    customer = Payjp::Customer.create(  ## 顧客の作成
+    email: current_user.email,
+    card: params[:payjp_token]
+    )
+    card = current_user.build_card(card_token: params[:card_token], customer_token: customer.id)
+    if card.save!
+      redirect_to card_path(current_user), notice: "カードの登録が完了しました。"
+    else
+      redirect_to new_card_path, alert: "カードの登録に失敗しました。"
+    end
+
+  end
 end
