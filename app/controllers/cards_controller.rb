3b226513f4e900f654fce6c862063df1bfc8283c
usagrammer <48462721+usagrammer@users.noreply.github.com>
2020-04-09 15:24:14 +0900

    change method save! to save

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -13,7 +13,7 @@ class CardsController < ApplicationController
     Payjp.api_key = Rails.application.credentials.payjp[:secret_key]
     customer = Payjp::Customer.create(card: params[:payjp_token]) ## 顧客の作成
     card = current_user.build_card(card_token: params[:card_token], customer_token: customer.id)
-    if card.save!
+    if card.save
       redirect_to card_path(current_user), notice: "カードの登録が完了しました。"
     else
       redirect_to new_card_path, alert: "カードの登録に失敗しました。"
