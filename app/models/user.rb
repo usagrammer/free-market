44c99804f8937b66e588bea3dfdededa8cff3c24
usagrammer <48462721+usagrammer@users.noreply.github.com>
2020-04-11 11:02:07 +0900

    add relation user and sns_credential

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,5 +12,6 @@ class User < ApplicationRecord
 
   has_many :selling_items, class_name: "Item", foreign_key: "seller_id"
   has_many :bought_items, class_name: "Item", foreign_key: "buyer_id"
+  has_one :sns_credential, dependent: :destroy
 
 end
