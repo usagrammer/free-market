f39fe07e2f45484a9836d5e00d6163de91f5acff
usagrammer <48462721+usagrammer@users.noreply.github.com>
2020-04-12 12:01:29 +0900

    add accepts_nested_attributes_for

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,6 +7,7 @@ class Item < ApplicationRecord
   belongs_to :seller, class_name: "User"
   belongs_to :buyer, class_name: "User", optional: :true
   has_many :images
+  accepts_nested_attributes_for :images
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
