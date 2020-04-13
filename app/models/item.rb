9cb1a7f0eca266cde009177c50cdec751b95b091
usagrammer <48462721+usagrammer@users.noreply.github.com>
2020-04-13 19:03:01 +0900

    add option for edit and destroy images

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,9 +5,9 @@ class Item < ApplicationRecord
 
   belongs_to :category
   belongs_to :seller, class_name: "User"
-  belongs_to :buyer, class_name: "User", optional: :true
+  belongs_to :buyer, class_name: "User", optional: true
   has_many :images
-  accepts_nested_attributes_for :images
+  accepts_nested_attributes_for :images, allow_destroy: true, update_only: true
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
