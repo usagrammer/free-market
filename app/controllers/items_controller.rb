1b22ae6e1d1bf5e2bd50f0a47993dc45d143d7c5
usagrammer <48462721+usagrammer@users.noreply.github.com>
2020-04-13 21:36:34 +0900

    change render to redirect_to

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -14,14 +14,14 @@ class ItemsController < ApplicationController
     if @item.save
       redirect_to root_path, notice: "出品に成功しました"
     else
-      render layout: 'no_menu', template: 'items/new' # レイアウトファイル指定
+      redirect_to new_item_path, alert: @item.errors.full_messages
     end
   end
 
   def edit
     @item = Item.find(params[:id])
     @item.images.build
-    render layout: 'no_menu' # レイアウトファイル指定
+    render layout: 'no_menu'  # レイアウトファイル指定
   end
 
   def update
@@ -29,7 +29,7 @@ class ItemsController < ApplicationController
     if @item.update(item_params)
       redirect_to root_path, notice: "出品に成功しました"
     else
-      render layout: 'no_menu', template: 'items/new' # レイアウトファイル指定
+      redirect_to edit_item_path(@item), alert: @item.errors.full_messages  ## ここを変更
     end
   end
 
