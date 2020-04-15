d175614df49295eca18f9e910e4180a8190dc712
usagrammer <48462721+usagrammer@users.noreply.github.com>
2020-04-15 10:38:27 +0900

    add dependent: :destroy

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,7 @@ class Item < ApplicationRecord
   belongs_to :category
   belongs_to :seller, class_name: "User"
   belongs_to :buyer, class_name: "User", optional: true
-  has_many :images
+  has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true, update_only: true
 
   extend ActiveHash::Associations::ActiveRecordExtensions
