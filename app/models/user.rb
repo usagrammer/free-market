21794d4459eceee8ac0aa68670bf12a2111a9dda
usagrammer <48462721+usagrammer@users.noreply.github.com>
2020-04-17 22:48:31 +0900

    change password validation for devise random password

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   validates :nickname, :birthday, :first_name, :last_name, presence: true
   validates :first_name_reading, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。'}
   validates :last_name_reading, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。'}
-  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,128}+\z/i }
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[\w-]{8,128}+\z/i }
 
   has_one :address
   has_many :selling_items, class_name: "Item", foreign_key: "seller_id"
