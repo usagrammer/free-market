f6d82c9ab95aa8516df570d32532551ba3c848fb
usagrammer <48462721+usagrammer@users.noreply.github.com>
2020-04-17 20:26:38 +0900

    add validation

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,8 @@
 class Address < ApplicationRecord
   belongs_to :user, optional: true
 
+  validates :prefecture_id, :city, :house_number, :postal_code, presence: true
+  validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
+  validates :phone_number, format: {with: /\A\d{10,11}\z/}, allow_blank: true
 
 end
