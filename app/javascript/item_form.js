15864557419f656c50783d1729d9a02431180eca
usagrammer <48462721+usagrammer@users.noreply.github.com>
2020-04-19 11:41:58 +0900

    add change file_fields event

--- app/javascript/item_form.js
+++ app/javascript/item_form.js
@@ -9,4 +9,20 @@ document.addEventListener('turbolinks:load', function () {
     file_field.trigger("click"); // file_fieldをクリックさせる。
   });
   /////////画像の投稿ボタン（グレーのブロック）をクリックした時ここまで/////////
+  /////////////////////////////////////////////
+  /////////file_fieldが変化した時/////////
+  ////////////////////////////////////////////
+
+  $(`#image-file-fields`).on("change", `input[type="file"]`, function (e) { //新しく画像が選択された、もしくは変更しようとしたが何も選択しなかった時
+    let file = e.target.files[0];
+    let blob = window.URL.createObjectURL(file); //選択された画像をblob形式に変換する。
+    let index = $(this).data("index");
+    index += 1;
+    let file_field_html = newFileField(index);
+    $("#image-file-fields").append(file_field_html);
+    let preview_html = `<img class="item-image new" src="${blob}" width="20%">`;
+    $("#select-image-button").before(preview_html);
+  });
+  /////////file_fieldが変化した時ここまで/////////
+
 });
