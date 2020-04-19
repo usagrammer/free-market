db669493c7fdaf1ec6d59292677a8f17ecb71352
usagrammer <48462721+usagrammer@users.noreply.github.com>
2020-04-19 11:41:37 +0900

    add click event image select button

--- app/javascript/item_form.js
+++ app/javascript/item_form.js
@@ -1,4 +1,12 @@
 document.addEventListener('turbolinks:load', function () {
   if (!$('#item_form')[0]) return false; //商品出品・編集ページではないなら以降実行しない。
 
+  ///////////////////////////////////////////////////////////////
+  /////////画像の投稿ボタン（グレーのブロック）をクリックした時。/////////
+  ///////////////////////////////////////////////////////////////
+  $("#select-image-button").on("click", function () {
+    const file_field = $(`.new-item-image:last`); // 新規画像投稿用のfile_fieldのを取得する。
+    file_field.trigger("click"); // file_fieldをクリックさせる。
+  });
+  /////////画像の投稿ボタン（グレーのブロック）をクリックした時ここまで/////////
 });
