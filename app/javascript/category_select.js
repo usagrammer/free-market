8d8d66c578169da210b731b1e3b57e3deac154f4
usagrammer <48462721+usagrammer@users.noreply.github.com>
2020-04-20 16:09:51 +0900

    add category change event

--- app/javascript/category_select.js
+++ app/javascript/category_select.js
@@ -1,3 +1,5 @@
 document.addEventListener('turbolinks:load', function () {
   if (!$('.select-category')[0]) return false; //カテゴリのフォームが無いなら以降実行しない。
+  $(".input-field-main").on("change", ".select-category", function () { //カテゴリが選択された時
+  });
 });
