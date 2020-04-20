492f690079d54914420b7326f5d72088787649cc
usagrammer <48462721+usagrammer@users.noreply.github.com>
2020-04-20 16:11:01 +0900

    add generate next form

--- app/javascript/category_select.js
+++ app/javascript/category_select.js
@@ -1,5 +1,24 @@
 document.addEventListener('turbolinks:load', function () {
   if (!$('.select-category')[0]) return false; //カテゴリのフォームが無いなら以降実行しない。
+
+  function buildCategoryForm(categories) { // 子孫カテゴリのフォームを組み立てる
+    let options = "";
+    categories.forEach(function (category) { // カテゴリを一つずつ渡してoptionタグを一つずつ組み立てていく。
+      options += `
+                  <option value="${category.id}">${category.name}</option>
+                 `;
+    });
+
+    const html = `
+                  <select required="required" class="select-category" id="parent-category" name="item[category_id]">
+                    <option value="">---</option>
+                    ${options}
+                  </select>
+                 `;
+    return html;
+  }
+  /////////buildCategoryForm()ここまで/////////
+
   $(".input-field-main").on("change", ".select-category", function () { //カテゴリが選択された時
     const category_id = $(this).val(); // 選択されたカテゴリのidを取得
     $.ajax({
@@ -10,6 +29,8 @@ document.addEventListener('turbolinks:load', function () {
         },
         dataType: 'json',
       }).done(function (categories) {
+        const html = buildCategoryForm(categories); // カテゴリのフォームを組み立てる
+        $(".select-category:last").after(html); // 組み立てたフォームを表示
       }.bind(this)) //  -----ここにbind(this)を追加するとajax後でもthisが維持される-----
       .fail(function () {
         alert('error');
