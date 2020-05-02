b696fa1bdde06e758f061c60e2d40015d9cf5f62
usagrammer <48462721+usagrammer@users.noreply.github.com>
2020-05-02 12:30:01 +0900

    add show action

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,4 +4,9 @@ class CategoriesController < ApplicationController
   def index
   end
 
+  def show
+    @category = Category.find(params[:id])
+    @items = Item.search_by_categories(@category.subtree_ids).order("created_at DESC").page(params[:page]).per(4)
+  end
+
 end
