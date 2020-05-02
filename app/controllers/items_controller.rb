c34368ee37d3c9309eee0a18318600c3c27ef108
usagrammer <48462721+usagrammer@users.noreply.github.com>
2020-05-02 10:52:03 +0900

    add show to before actions

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index]
-  before_action :set_item, only: [:edit, :update, :destroy]
+  skip_before_action :authenticate_user!, only: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :move_to_index, only: [:edit, :update, :destroy], unless: :seller?
 
   def new
