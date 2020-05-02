828ddf246bd49a7ca1307c0aaa5badfad8ed4e66
usagrammer <48462721+usagrammer@users.noreply.github.com>
2020-05-02 12:27:53 +0900

    add scope and method to search items

--- app/models/item.rb
+++ app/models/item.rb
@@ -50,7 +50,13 @@ class Item < ApplicationRecord
     "売り切れ": 1
     }
 
-  private
+  scope :new_items, -> { order("created_at DESC").limit(4) }
+
+  def self.search_by_categories(category_ids)
+    return Item.where(category_id: category_ids).includes(:images)
+  end
+
+    private
 
   def images_range
     errors.add(:images, "を1枚以上選択してください") if images.size < 1
