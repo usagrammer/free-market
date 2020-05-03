ba30b188dffcef294db5e1f310fd48c4f666f539
usagrammer <48462721+usagrammer@users.noreply.github.com>
2020-05-03 19:53:11 +0900

    change rename categories migration file

--- db/schema.rb
+++ db/schema.rb
@@ -114,6 +114,15 @@ ActiveRecord::Schema.define(version: 2019_09_09_080152) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "addresses", "users"
+  add_foreign_key "cards", "users"
+  add_foreign_key "comments", "items"
+  add_foreign_key "comments", "users"
+  add_foreign_key "images", "items"
+  add_foreign_key "items", "categories"
   add_foreign_key "items", "users", column: "buyer_id"
   add_foreign_key "items", "users", column: "seller_id"
+  add_foreign_key "likes", "items"
+  add_foreign_key "likes", "users"
+  add_foreign_key "sns_credentials", "users"
 end
