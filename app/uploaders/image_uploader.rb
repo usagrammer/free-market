3988f612b3cf43019f25a8a61d4b4f4f72fc2193
usagrammer <48462721+usagrammer@users.noreply.github.com>
2020-05-06 18:41:04 +0900

    change image storage when production or development

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -6,7 +6,11 @@ class ImageUploader < CarrierWave::Uploader::Base
   process resize_to_limit: [800, 800]
 
   # Choose what kind of storage to use for this uploader:
-  storage :fog
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
